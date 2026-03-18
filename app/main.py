54f1734bce44ecaf615fdfd19d3c1e0b68e2d70c
williamdev20 <williamalvs45@gmail.com>
2026-03-18 20:58:09 -0300

    feat: Adicionar configuração de CORS

--- app/main.py
+++ app/main.py
@@ -1,5 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import router
 
 app = FastAPI(title="ChecAI API")
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 app.include_router(router)
