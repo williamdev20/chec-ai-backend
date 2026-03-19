b7f497a83c38627cace0c1de7273f29689f68b3d
williamdev20 <williamalvs45@gmail.com>
2026-03-19 00:39:33 -0300

    fix: Corrigir CORS e tamanho máximo permitido da imagem

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter, UploadFile, File, HTTPException
 from io import BytesIO
 from app.services.check_poster_service import check_poster
 
@@ -7,6 +7,9 @@ router = APIRouter()
 @router.post("/check")
 async def check_fake_news(img: UploadFile = File(...)):
     img_bytes = await img.read()
+    if len(img_bytes) > 50 * 1024 * 1024:  # 50MB
+        raise HTTPException(status_code=413, detail="Tamanho da imagem é maior que 50MB")
+
     img_stream = BytesIO(img_bytes)
 
     result = check_poster(img_stream)
